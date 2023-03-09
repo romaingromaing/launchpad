538b0ec08306bd79195895fc66cec57068b68402
kasodesyn <bokang.qian@desyn.io>
2023-03-09 15:32:07 +0800

    i

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv/config');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.12",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      saveDeployments: true,
+      blockGasLimit: 21000000,
+
+    },
+    localhost: {
+      live: false,
+      url: "http://localhost:8545",
+      saveDeployments: true,
+  
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+      saveDeployments: true,
+    },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+      saveDeployments: true,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_APIKEY,
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,21 +7,12 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const MainContract = await hre.ethers.getContractFactory("Main");
+  const mainContract = await MainContract.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await mainContract.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(mainContract.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
